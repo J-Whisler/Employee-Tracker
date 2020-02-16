8c4fd9f44d014744a69522b7e53fba6831424c6b
Jacob Whisler <jwhisler521@gmail.com>
2020-02-16 10:39:01 -0500

    added add, view all functions for employeee and roles

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
 const inquirer = require('inquirer')
-const express = require('express')
-const mysql = require('mysql')
 
-const PORT = process.env.PORT || 3000;
+const mysql = require('mysql')
+const consoleTable = require('console.table')
+
+const questions = require('./question')
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
-    database: 'employee_trackerDB'
+    password: 'root521',
+    database: 'employeeDB'
 });
 
-connection.connect(function(res, err) {
-    if (err) throw err;
-    console.log(res)
-    userPrompt()
+connection.connect(err => {
+    if (err) throw err;   
 })
 
-function userPrompt () {
-    inquirer
-        .prompt({
-            name: 'action',
-            type: 'rawlist',
-            message: 'What would you like to do?',
-            choices: [
-                'View all employees',
-                'View all employees by department',
-                'View all employees by manager',
-                'Add an employee',
-                'Remove an employee',
-                'Update employee role',
-                'Update manager role',
-                'Update employee manager',
-                'View all roles',
-                'Add a role',
-                'Remove a role' 
-            ]
-        })
-        .then (answer => {
-            switch (answer.userPrompt) {
+userPrompt()
+async function userPrompt () {
+    const results = await inquirer.prompt(questions.prompts);
+        switch (results.prompts) {
                 case 'View all employees':
                     viewAllEmployees();
                     break;
@@ -79,7 +62,122 @@ function userPrompt () {
                 case 'Remove role':
                     removeRole();
                     break;
-            }
-        })
+
+                default:
+                    connection.end()
+                    break;
+        }      
+} 
+
+ function viewAllEmployees () {
+    connection
+        .query(`SELECT employee.employeeID, employee.firstName, employee.lastName, department.departmentName, role.title, role.salary
+        FROM employee
+        LEFT JOIN role ON employee.employeeRole = role.roleID
+        LEFT JOIN department ON role.departmentId = department.departmentID
+        ORDER BY employee.employeeID`, (err, res) => {
+    
+    console.table(res)
+    if (err) throw err;
+    userPrompt()
+  });
 }
 
+function addEmployee () {
+    connection
+        .query(`SELECT * FROM employee`, (err, results) => {
+            if (err) throw err
+
+            inquirer
+                .prompt([
+                    {
+                        name: 'firstName',
+                        type: 'input',
+                        message: 'Enter the first name of the new employee'
+                    },
+                    {
+                        name: 'lastName',
+                        type: 'input',
+                        message: 'Enter the last name of the new employee'
+                    },
+                    {
+                        name: 'roleID',
+                        type: 'number',
+                        message: `Enter the new employee\'s role id 
+                        (1 = Sales Lead, 2 = Sales Person, 3 = Lead Engineer, 4 = Software Engineer, 5 = Accountant, 6 = Legal Team Lead, 7 = Lawyer)
+                        `,    
+                    }
+                ])
+                .then((res) => {
+                    let query = `INSERT INTO employee SET ?`
+                    const values = {
+                        firstName: res.firstName,
+                        lastName: res.lastName,
+                        employeeRole:res.roleID,
+                        // title: res.title,
+                        // salary: res.salary
+                    }
+                 connection.query(query, values, (err) => {
+                     if (err) throw err;
+                     console.log('New employee added!')
+                    //  console.table(results)
+                     userPrompt();
+                 })
+                    // console.log(res)
+                })    
+    }
+  )
+}
+
+
+
+
+function viewAllRoles () {
+    connection.query(`SELECT title FROM role`, (err, res) => {
+        console.table(res)
+        if (err) throw err;
+        userPrompt();
+    })
+}
+
+function addRole () {
+    connection.query(`SELECT * FROM role`, (err, res) => {
+        if (err) throw err
+
+        inquirer
+            .prompt([
+                {
+                    name: 'newRoleTitle',
+                    type: 'input',
+                    message: 'Enter the the of the role you would like to add'
+                },
+                {
+                    name: 'newRoleID',
+                    type: 'number',
+                    message: 'Enter the ID you would like to give this role.  Make sure the ID is not being used for another role'
+                },
+                {
+                    name: 'newRoleSalary',
+                    type: 'number',
+                    message: 'Enter the salary of the new role'
+                }
+            ])
+            .then((res) => {
+                let query = `INSERT INTO role SET ?`
+                const values = {
+                    title: res.newRoleTitle,
+                    roleID: res.newRole,
+                    salary: res.newRoleSalary
+                }
+                connection.query(query, values, (err) => {
+                    if (err) throw err
+                    console.log('New role added!')
+                    userPrompt();
+                })
+            }
+    )})
+}
+
+
+// console.table(viewAllEmployees())
+// console.table(['hello', 'asaasf', 'sgdsg'])
--- seeds.sql
+++ seeds.sql
@@ -1,28 +1,62 @@
-DROP DATABASE IF EXISTS employee_trackerdb;
-CREATE database employee_trackerdb;
+DROP DATABASE IF EXISTS employeeDB;
+CREATE database employeeDB;
 
-USE employee_trackerdb;
+USE employeeDB;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  departmentID INT NOT NULL AUTO_INCREMENT,
+  departmentName VARCHAR(30) NOT NULL,
+  PRIMARY KEY (departmentID)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
+  roleID INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DEC(10, 4) NOT NULL,
-  department_id INT(10, 4) NOT NULL,
-  PRIMARY KEY(id)
+  salary DEC NOT NULL,
+  departmentID INT,
+  PRIMARY KEY(roleID),
+  CONSTRAINT FK_department
+    FOREIGN KEY (departmentID) REFERENCES department(departmentID),
+ 
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT (10, 4) NOT NULL,
-    manager_id INT (10, 4) NOT NULL,
-    PRIMARY KEY(id)
+    employeeID INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    employeeRole INT,
+    PRIMARY KEY(employeeID),
+    CONSTRAINT FK_role FOREIGN KEY (employeeRole)
+    REFERENCES role(roleID),
+    managerID INT,
+    CONSTRAINT FK_manager
+    FOREIGN KEY (managerID)
+    REFERENCES employee(employeeID)
+    
 )
+
+INSERT INTO department(department_name)
+VALUES ('Engineering'), ('Sales'), ('Finance'), ('Legal')
+
+INSERT INTO role(title, salary, department_id)
+VALUES ('Sales lead', 100000, 2), 
+       ('Sales person', 80000, 2), 
+       ('Lead Engineer', 150000, 1), 
+       ('Software engineer', 120000, 1), 
+       ('Accountant', 125000, 3),
+       ('Legal Team Lead', 250000, 4),
+       ('Lawyer' 190000, 4)
+
+INSERT INTO employee(firstName, lastName, employeeRole, managerID)
+VALUES ('Jacob', 'Whisler', 1, 1),
+       ('Michael', 'Scott', 2, 1),
+       ('Dwight', 'Shrute', 3, 2),
+       ('Pam', 'Beasley', 3, 2),
+       ('Jim', 'Halpert', 3, 2),
+       ('Kevin', 'Malone', 4, 3),
+       ('Angela', 'Martin', 4, 3),
+       ('Ryan', 'Howard', 5, 4),
+       ('Creed', 'Bratton', 6, 4);
+
+
 
